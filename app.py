b2fb5cd805f98aa9e0e59daf3550c9b76d7487fa
emmadickson <emma.jk.dickson@gmail.com>
2017-08-30 21:16:06 -0400

    de

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ def render_static():
     return app.send_static_file('index.html')
 
 @app.route('/db')
-def render_static():
+def render_statics():
     return app.send_static_file('db.html')
 
 @app.route('/raw')
-def render_static():
+def render_staticd():
     return app.send_static_file('db.txt')
-    
+
 @app.route("/bots")
 def webscrape():
     # 1. Specify the local url
