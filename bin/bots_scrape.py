069575fd2f55f3cf91ec061ef8334f978e0c1734
emmadickson <emma.jk.dickson@gmail.com>
2017-10-01 12:21:12 -0400

    db wipe and visual fix

--- bin/bots_scrape.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def bots_update():
-  r = requests.get('https://mixed-connection.herokuapp.com/bots', auth=('user', 'pass'))
-  r.status_code
-  print (r.status_code)
-
-bots_update()
