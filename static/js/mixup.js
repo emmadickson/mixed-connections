a55bbbffa31ff5ba5d59c494c11b9b7eea0758c7
emmadickson <emma.jk.dickson@gmail.com>
2017-10-06 09:41:06 -0400

    tetst

--- static/js/mixup.js
+++ static/js/mixup.js
@@ -96,7 +96,7 @@ function scramble(originalText, domElement){
 function makeid() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  for (var i = 0; i < 2; i++)
+  for (var i = 0; i < 6; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
 }
