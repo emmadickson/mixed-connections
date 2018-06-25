fc3a07f4aa3eaa52ce89eb6a396b453bc54f82a5
Emma Dickson <ejd315@nyu.edu>
2018-06-25 15:48:45 -0400

    test postgres

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 from flask import Flask
 from flask import request
 import requests
-import socks
-import socket
-import bs4
-import hashlib
-import re
-import json
-import datetime
-from random import shuffle
-import random
-import os
-from PIL import Image
-import urllib2
-import cStringIO
-import subprocess
+import psycopg2
+
 
 app = Flask(__name__, static_url_path='')
 
@@ -28,6 +16,15 @@ def render_index():
 
 @app.route('/raw_db')
 def render_db():
+    DATABASE_URL='postgres://pkszoedlaykwsk:2ff4fae6161d29c22cf40f349faaa1e48d8524aab1caf6eed72f773a31f0a91b@ec2-54-83-0-158.compute-1.amazonaws.com:5432/d42mu98rpmdqbj'
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require', user='pkszoedlaykwsk', password='2ff4fae6161d29c22cf40f349faaa1e48d8524aab1caf6eed72f773a31f0a91b' )
+    cursor = conn.cursor()
+    query =  "SELECT * FROM posts_scraped;"
+    print(DATABASE_URL)
+    posts = cursor.execute(query)
+    print("Retrieved Posts")
+    print(posts)
+
     return app.send_static_file('data/db.json')
 
 if __name__ == '__main__':
--- static/bash/commit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git add static/
-git commit -m '!'
-git push origin master -f
