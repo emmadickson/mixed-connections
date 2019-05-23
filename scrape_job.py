fdb35479e018c07d6a40f68568de578a6b1db6ee
Emma Dickson <ejd315@nyu.edu>
2019-05-23 15:08:07 -0400

    Update scrape_job.py

--- scrape_job.py
+++ scrape_job.py
@@ -19,7 +19,7 @@ CRAIGSLIST_URLS = [
 "https://pittsburgh.craigslist.org"
 ]
 
-NUMBER_OF_POSTS = 5
+NUMBER_OF_POSTS = 15
 DATABASE_URL='postgres://pkszoedlaykwsk:2ff4fae6161d29c22cf40f349faaa1e48d8524aab1caf6eed72f773a31f0a91b@ec2-54-83-0-158.compute-1.amazonaws.com:5432/d42mu98rpmdqbj'
 
 
