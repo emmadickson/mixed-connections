0b4e5a1001ce01609c21040a919fe4f0071b6195
emmadickson <emma.dickson@artsymail.com>
2023-03-22 22:55:49 -0400

    scrape

--- scrape_job.py
+++ scrape_job.py
@@ -48,7 +48,7 @@ def CollectMissedConnectionsLink(location):
 
 
     driver.get(randomCraigslistUrl)
-    content = driver.find_elements_by_tag_name('a')
+    content = driver.find_element("tag", "a")
     for elem in elems:
         href = elem.get_attribute('href')
         if href is not None:
